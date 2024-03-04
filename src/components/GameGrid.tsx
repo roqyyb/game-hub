c6e5332a61811a8d525826ae458661d029a4e743
roqyyb <roqtund@gmail.com>
2024-03-04 11:40:38 +0100

    Create the fecth game hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import axios from "axios";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => {
-        console.log(res.data.results);
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        console.log(err.message);
-        setError(err.message);
-      });
-  }, []);
+  const { error, games } = useGames();
 
   return (
     <>
