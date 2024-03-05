0152fc0a9ef0d63c85c404fae7d122edd928c054
roqyyb <roqtund@gmail.com>
2024-03-05 15:42:51 +0100

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+function CriticScore({ score }: Props) {
+  let color = score > 75 ? "green" : score > 60 ? "yello" : "";
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2}>
+      {score}
+    </Badge>
+  );
+}
+
+export default CriticScore;
