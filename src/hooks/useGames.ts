619ced349e66a5f40341f04efe33a9ea2c34e68e
roqyyb <roqtund@gmail.com>
2024-03-29 19:59:54 +0100

    Refactor- simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
 import APIClient from "../services/api-client";
 import { Platform } from "./usePlatorms";
@@ -34,7 +35,7 @@ const useGames = (gameQuery: GameQuery) =>
       return lastPage.next ? allPages.length + 1 : undefined;
     },
 
-    staleTime: 24 * 60 * 60 * 100, //24h
+    staleTime: ms("24h"),
   });
 
 export default useGames;
