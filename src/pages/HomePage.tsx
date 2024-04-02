7fae035798c4e62ca0fa42f18c4f794fb1e387cb
roqyyb <roqtund@gmail.com>
2024-04-02 09:59:05 +0100

    Setup routing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,26 +1,22 @@
-import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
+import { Box, Grid, Show, GridItem, HStack } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
 
-function App() {
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `'nav' "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: ` "main"`,
+        lg: ` "aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      <GridItem area={"nav"}>
-        <NavBar />
-      </GridItem>
       <Show above="lg">
         <GridItem paddingX={5} area={"aside"}>
           <GenreList />
@@ -39,6 +35,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
