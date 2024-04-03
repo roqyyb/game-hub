ff92f0ddb563a5b4f51291a32985303bbeb262eb
roqyyb <roqtund@gmail.com>
2024-04-03 17:50:13 +0100

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 import GameTrailers from "../components/GameTrailers";
+import GameScreenshots from "../components/GameScreenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -18,6 +19,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game?.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailers gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
